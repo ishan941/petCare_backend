f154425a024df45de5135f0304c1654d6be2c72f
Sulav Manandhar <manandhsulav07@gmail.com>
2024-09-08 22:08:54 +0545

    Fixed paths

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -1,27 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from 'src/core /App/app.controller';
-import { AppService } from 'src/core /App/app.service';
+
 import { AuthModule } from './Auth/auth.module';
 import { PrismaModule } from './Prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AdsModuleModule } from './ads-module/ads-module.module';
+import { AppController } from 'src/core/App/app.controller';
+import { AppService } from 'src/core/App/app.service';
 
 @Module({
-
-    imports: [
-        ConfigModule.forRoot({
-            isGlobal: true,
-        }),
-        AuthModule,
-        PrismaModule,
-        JwtModule,
-        AdsModuleModule
-
-    ],
-    controllers: [AppController],
-    providers: [AppService,],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    PrismaModule,
+    JwtModule,
+    AdsModuleModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class ModulesModule {
-
-}
+export class ModulesModule {}
