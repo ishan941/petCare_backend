2915c6f61561c0edf736b426d1d74d30a60e6d82
Shakti Tamang <sushrabshaktiamu@gmail.com>
2024-09-08 22:35:11 +0545

    bug fix

--- src/modules/ads-module/ads-module.module.ts
+++ src/modules/ads-module/ads-module.module.ts
@@ -3,12 +3,16 @@ import { AdsController } from './controllers/ads.controller';
 import { AdsServiceService } from './service/ads-service.service';
 import { AdsRepo } from './Repo/ads.repo';
 import { ConfigModule } from '@nestjs/config';
-import { PGDatabaseService } from 'src/core /Database/pg.database.service';
+
 import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { join } from 'path';
+import { PGDatabaseService } from 'src/core /Database/pg.database.service';
 
 @Module({
   imports: [
+
+    // file saving
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'uploads'), // Path to your uploaded files
       serveRoot: '/uploads', // URL path to access the files
